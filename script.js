9192112dcfa080a4f322d2ccdac08e6c3591010f
Setyo Adi Sasono <echo.setyoadisasono@gmail.com>
2024-07-13 20:31:22 +0700

    add about section

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toogleMenu() {
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
